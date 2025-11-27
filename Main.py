85f2c603a72dec1b20db866734ac0b6c340ba410
lorddrago12 <clearrio1465@gmail.com>
2025-11-27 19:22:56 +0530

    Implement player and computer moves in Tic-Tac-Toe
    Added player and computer turn functionality for Tic-Tac-Toe.

--- Main.py
+++ Main.py
@@ -1,6 +1,13 @@
+import random
+
 Board = ["-", "-", "-",
          "-", "-", "-",
          "-", "-", "-"]
+
+currentPlayer = "X"
+winner = None
+gameRunning = True
+ 
 
 def printBoard(board):
     print(board[0] + " | " + board[1] + " | " + board[2])
@@ -9,6 +16,33 @@ def printBoard(board):
     print("---------")
     print(board[6] + " | " + board[7] + " | " + board[8])
 
-printBoard(Board)
 
-# Not Completed Yet
+def playerInput(board):
+    inp = int(input("Enter a number from 1-9 to place your X: "))
+    if inp >= 1 and inp <= 9 and board[inp - 1] == "-":
+        board[inp - 1] = "X"
+    else:
+        print("Opps player has already placed their mark there. Try again.")
+        playerInput(board)
+
+
+def computerMove(board):
+    available = [i for i, v in enumerate(board) if v == "-"]
+    move = random.choice(available)
+    board[move] = "O"
+    print(f"Computer placed O in position {move+1}")
+
+
+# --- THE LOOP YOU WANTED ---
+while gameRunning:
+    printBoard(Board)
+
+    # Player turn
+    playerInput(Board)
+
+    printBoard(Board)
+
+    # Computer turn
+    computerMove(Board)
+
+# Not added the check win, lose or draw function.
